27042b6f801fde3db2f79f68b208f0f164facf95
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-13 18:53:28 -0400

    Updated App.js, App.css,  Answers.css and Answers.css
    To update the Answers function

---
+++ src/Answers.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./Answers.css";
+
+function Answers() {
+  const [userInput, setUserInput] = useState("");
+  const [randomIndex, setRandomIndex] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (userInput) {
+      setError("");
+      setRandomIndex(Math.round(Math.random() * 20));
+      setUserInput("");
+    } else {
+      setError("Enter a question to know the answer!");
+    }
+    console.log(randomIndex);
+  };
+
+  const possibleAnswers = [
+    "I cannot answer that question.",
+    "Dare to dream!",
+    "It is certain.",
+    "Don't count on it.",
+    "It is decidedly so!",
+    "A miracle is on its way!",
+    "Follow your heart!",
+    "Appreciate present blessing.",
+    "Listen to your intuition!",
+    "Outlook not so good.",
+    "Signs point to yes.",
+    "Only time will tell.",
+    "It's time to let go.",
+    "It is time to act.",
+    "You are divinely guided. Don't give up!",
+    "Keep your guard up!",
+    "All is an illusion.",
+    "Give yourself some time to heal.",
+    "The answer is no.",
+    "It is never too late.",
+  ];
+
+  const answer = possibleAnswers[randomIndex];
+  return (
+    <div className="App">
+      <input
+        type="text"
+        className="question"
+        value={userInput}
+        onChange={handleChange}
+      />
+      <div className="eight-ball">
+        <div className="content" onClick={handleClick}>
+          {error ? (
+            <p className="error">{error}</p>
+          ) : answer ? (
+            <p className="answer">{answer}</p>
+          ) : (
+            <button type="submit">get answer</button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Answers;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Ask from "./Ask";
-import Answer from "./Answer";
+import Answers from "./Answers";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Ask a question!</h1>
-        <Ask />
-        <h4>Let the Universe guide you. </h4>
-        <Answer />
+        <h1>Let the Universe guide you.</h1>
+
+        <h4>Ask the magic crystal ball a question!</h4>
+        <Answers />
       </header>
     </div>
   );
