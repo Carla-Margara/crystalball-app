677477db7918df946b899af2b6cf1e800a08ea66
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-13 13:04:29 -0400

    Updated App.js
    Added an h4 line.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Ask a question!</h1>
+        <h4>Make sure it is a "yes" or "no" question. </h4>
       </header>
     </div>
   );
