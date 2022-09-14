6c38c29f15677097b19b2b50ae19706c5a0b8cfa
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-13 21:55:00 -0400

    Updated App.css and Answers.css
    To change the overall styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Let the Universe guide you.</h1>
+        <h1>Let the Universe guide you</h1>
 
-        <h4>Type to ask the magic crystal ball a question!</h4>
+        <h3>Type to ask the magic crystal ball a question!</h3>
         <Answers />
       </header>
     </div>
