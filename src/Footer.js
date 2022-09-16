b8f8c8bce29389341c92c2bd4e10a2bd7e4ee486
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-16 16:28:14 -0400

    Updated Footer.js and Footer.css

--- src/Footer.js
+++ src/Footer.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <div className="signature">Footer</div>;
+  return (
+    <div className="signature">
+      Hosted with <a href="https://www.netlify.com">Netlify</a> |{" "}
+      <a
+        href="https://github.com/Carla-Margara/crystalball-app"
+        title="GitHub Repository link"
+      >
+        Open-Sourced and Coded
+      </a>{" "}
+      with love by Carla Margara.
+    </div>
+  );
 }
