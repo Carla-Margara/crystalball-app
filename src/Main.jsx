317d0d1be668315be2b275c2c0c171af9c94a1b7
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-16 15:46:44 -0400

    Eliminated overlay element on Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,6 @@ import cloud from "./media/blue-universe-trim.mp4";
 const Main = () => {
   return (
     <div className="main">
-      <div className="overlay"></div>
       <video src={cloud} autoPlay loop muted />
     </div>
   );
