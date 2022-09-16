4d686be9712cc691bfd2b9eb816227d439eac9c7
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-09-16 15:54:00 -0400

    Added credit footer on App.js
    And updated the App.css

--- src/App.js
+++ src/App.js
@@ -10,6 +10,28 @@ function App() {
         <h3>Type to ask the Magic Crystal Ball a question!</h3>
         <Answers />
       </header>
+      <footer className="App-footer">
+        <span>
+          Stock footage provided by{" "}
+          <a
+            class="link author-link-popup"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.videvo.net/profile/videvo"
+          >
+            Videvo
+          </a>
+          , downloaded from{" "}
+          <a
+            class="videvo-redirect"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.videvo.net"
+          >
+            videvo.net
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }
